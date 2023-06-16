704b70cdea0777fbd9da8846a8ecae35ece0d6c2
kamalraj-ten <66177629+kamalraj-ten@users.noreply.github.com>
2023-06-16 12:03:37 +0530

    Update index.py to pass or fail randomly

--- index.py
+++ index.py
@@ -1,7 +1,15 @@
 # This is a python script file
+import random
+import sys
 
 if __name__ == '__main__':
   print("Hello Jenkins!")
   
   for i in range(10):
     print(f"Count: {i}")
+    
+  # trying to pass or fail randomly
+  passed = [True, False]
+  if not random.choice( passed ):
+    # exit with a string val
+    sys.exit("Failed")
